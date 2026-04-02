56bfef1384701afd6915a43b40feea28f3dbd472
João Gabriel De Oliveira Arantes <141735198+Jaoarantes@users.noreply.github.com>
2026-04-02 16:43:45 -0300

    .

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.money.app',
+  appName: 'Money',
+  webDir: 'dist'
+};
+
+export default config;
